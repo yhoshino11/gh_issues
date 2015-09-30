8d0708db28b0fbd9d2f1c4bbeae5e7a38511f657
Yu Hoshino <yhoshino11@gmail.com>
2015-09-30 22:58:56 +0900

    Take first n items

--- lib/gh_issues/cli.ex
+++ lib/gh_issues/cli.ex
@@ -19,11 +19,12 @@ defmodule GHIssues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     GHIssues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_hashdicts
     |> sort_into_ascending_order
+    |> Enum.take(count)
   end
 
   def sort_into_ascending_order(list_of_issues) do
