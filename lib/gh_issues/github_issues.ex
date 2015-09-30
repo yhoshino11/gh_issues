d90f53f244b9e52b15098eb465e5ef682fe7cfd1
Yu Hoshino <yhoshino11@gmail.com>
2015-09-30 22:40:03 +0900

    Fetch Github Issues for Open Source Project

---
+++ lib/gh_issues/github_issues.ex
@@ -0,0 +1,16 @@
+defmodule GHIssues.GithubIssues do
+  @user_agent [{ "User-agent", "yhoshino11@gmail.com"}]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response(%{status_code: 200, body: body}), do: { :ok, body }
+  def handle_response(%{status_code: _, body: body}), do: { :error, body }
+end
